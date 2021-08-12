2253f49fd7c3be390a8d0531155dc817fb263947
unknown <fatima.toloza@anima.edu.uy>
2021-08-12 08:34:25 -0300

    archivos nuevos

--- js.js
+++ js.js
@@ -1,5 +1,31 @@
-<button onclick="miBoton()">Hazme click</button>
-const miBoton=()=>{
-    window.navigator.vibrate([200,100,200,100,200])
-    alert('🐰🐥')
-}
+function calculadora() {
+    let tipoNafta = document.getElementById("select1").value;
+    let l_km = document.getElementById("l/km").value;
+    let km = document.getElementById("km").value;
+    let precioNafta = 0;
+  
+    switch (tipoNafta) {
+      case "1":
+        precioNafta = 65.47;
+        break;
+      case "2":
+        precioNafta = 67.80;
+        break;
+      case "3":
+        precioNafta = 45.29;
+        break;
+      case "4":
+        precioNafta = 67.87;
+        break;
+    }
+  
+    let naftaRequerida = (km * 1) / l_km;
+    document.getElementById(
+      "naftaNecesaria"
+    ).innerHTML = `${naftaRequerida} litros de nafta`;
+  
+    let costoViaje = naftaRequerida * precioNafta;
+    document.getElementById(
+      "costoViaje"
+    ).innerHTML = `<strong>$</strong>${costoViaje}`;
+  }
